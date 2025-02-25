289eed354916cbb35b0a5b8b721497ef8616e09b
Dan Souza <darleyble3@gmail.com>
2025-02-24 23:58:23 -0300

    components page test

--- src/app/components-page/page.tsx
+++ src/app/components-page/page.tsx
@@ -1,16 +1,10 @@
-import Markdown from "react-markdown";
-import Navbar from "@/app/components/components-test/navbar";
-import Button from "@/app/components/components-test/button";
+import Form from "@/app/components/components-test/form";
+import Alert from "@/app/components/components-test/alerts";
 import Card from "@/app/components/components-test/card";
 import Link from "next/link";
 
-const content = `# Lista de Componentes
-
-Esta é uma lista de componentes que podem ser integrados em sites utilizando a técnologia Tailwind. Foram dispostos diversos deles, das mais variadas cores 
-para a realização do teste de recoloração do filtro`;
-
-const lorem: string =
-  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod fugiat a eveniet id iusto saepe, voluptatem minima. Sed enim blanditiis possimus impedit corrupti consequatur ut atque exercitationem perspiciatis ad. Deleniti animi facere, perferendis nesciunt harum laboriosam, a autem odit ea doloremque quae quidem quaerat id quia aliquam suscipit rerum ad.";
+import { cards } from "./items-page";
+import { alerts } from "./items-page";
 
 const Page = async () => {
   return (
@@ -25,50 +19,69 @@ const Page = async () => {
         </p>
       </div>
       <div>
-        <h2 className="text-gray-800">Cards</h2>
-        <p className="text-gray-700">
-          Muitos sites de notícias, como o {" "}
-          <Link
-            className="text-blue-600"
-            href="https://globo.com"
-            target="_blank"
-          >
-            globo.com
-          </Link>
-          , utiliza das cores para indicar o tipo de notícia. Devido a isso,
-          pessoas com condição de daltonismo que frequentam esses tipos de site
-          perdem informações visuais que são passadas através das cores.
-        </p>
-        <p className="text-gray-700">
-          A seguir temos alguns exemplos de cards de notícias com cores para
-          transmitir informações. A partir deles, podemos ter uma noção de como
-          pessoas com daltonismo podem confundir as cores.
-        </p>
-        <div className="flex flex-col gap-4 lg:gap-3 lg:flex-row">
-          <Card title="Política" description={lorem} color="bg-[#c4170c]" />
-          <Card title="Esportes" description={lorem} color="bg-[#06aa48]" />
-          <Card title="Cultura" description={lorem} color="bg-[#Ff6700]" />
+        <div>
+          <h2 className="text-gray-800">Cards</h2>
+          <p className="text-gray-700">
+            Muitos sites de notícias, como o{" "}
+            <Link
+              className="text-blue-600"
+              href="https://globo.com"
+              target="_blank"
+            >
+              globo.com
+            </Link>
+            , utiliza das cores para indicar o tipo de notícia. Devido a isso,
+            pessoas com condição de daltonismo que frequentam esses tipos de
+            site perdem informações visuais que são passadas através das cores.
+          </p>
+          <p className="text-gray-700">
+            A seguir temos alguns exemplos de cards de notícias com cores para
+            transmitir informações. A partir deles, podemos ter uma noção de
+            como pessoas com daltonismo podem confundir as cores.
+          </p>
+        </div>
+        <div className="grid lg:grid-cols-3 gap-4 lg:gap-3 lg:flex-row">
+          {cards.map((card) => (
+            <Card
+              key={card.type}
+              type={card.type}
+              description={card.description}
+              color={card.bgColor}
+            />
+          ))}
         </div>
       </div>
-      <div>
-        <h2 className="text-xl mb-4">Buttons</h2>
-        <div className="flex flex-wrap gap-4">
-          <Button className="bg-[#31F533]" />
-          <Button className="bg-[#FFFF00]" />
-          <Button className="bg-[#D51A24]" />
-          <Button className="bg-[#31ADF5]" />
+      <div className="pt-5">
+        <div>
+          <h2 className="text-gray-800">Alerts</h2>
+          <p className="text-gray-700">
+            Outra forma muito comum de passar informações através das cores, é
+            utilizando alertas em relação a ações realizadas em um site, que
+            pode ocasionar o retorno de um status por uma API por exemplo.
+          </p>
+        </div>
+        <div className="grid lg:grid-cols-3 gap-4 lg:gap-3 lg:flex-row">
+          {alerts.map((alert) => (
+            <Alert
+              key={alert.type}
+              description={alert.description}
+              bgColor={alert.bgColor}
+              type={alert.type}
+            />
+          ))}
         </div>
       </div>
 
-      <div className="text-xl mb-4">
-        <h2>Navbars</h2>
-        <div>
-          <Navbar className="bg-[#31F533]" />
-          <Navbar className="bg-[#FFFF00]" />
-          <Navbar className="bg-[#D51A24]" />
-          <Navbar className="bg-[#31ADF5]" />
-        </div>
+      <div className="mb-4">
+        <h2 className="text-gray-800">Formulários</h2>
+        <p className="text-gray-700">
+          Cores em formulário também é uma das formas de passar informações ao
+          usuário, como por exemplo, indicar se um campo está com o dado correto
+          ou errado. Desse modo, o usuário saberá se a informação no campo está
+          de acordo com a validação do formulário.
+        </p>
       </div>
+      <Form />
     </section>
   );
 };
