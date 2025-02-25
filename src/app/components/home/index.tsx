97dcabe396b68d363864103d174b22a63a7dc98b
Dan Souza <darleyble3@gmail.com>
2025-02-24 23:57:19 -0300

    home page added

---
+++ src/app/components/home/index.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Home = async () => {
+  return (
+   <section className="container mx-auto px-24 mb-10 lg:prose-lg dark:prose-invert mt-20">
+    Home
+   </section>
+  );
+};
+
+export default Home;
